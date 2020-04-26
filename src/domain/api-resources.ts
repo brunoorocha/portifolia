4d319ae59d3625304c5e57805d9d715db79d0b16
Bruno Rocha <mail.brunorocha@gmail.com>
2020-04-26 19:00:16 -0300

    Removing a lost console.log

--- src/domain/api-resources.ts
+++ src/domain/api-resources.ts
@@ -1,7 +1,3 @@
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-console.log(process.env)
 
 export const DribbbreApiResources = {
   baseURL: process.env.REACT_APP_API_URL,
--- src/store/project/actions.ts
+++ src/store/project/actions.ts
@@ -7,7 +7,7 @@ export const fetchAllProjects = () => action(ProjectActionTypes.FETCH_ALL_START)
 
 export const fetchAllProjectsEnd = () => action(ProjectActionTypes.FETCH_ALL_END)
 
-export const setProjectsFeed = (projects: Project[]) => action(ProjectActionTypes.SET_PROJECTS_FEED, { projects })
+export const setProjectsFeed = (projects: Project[] = []) => action(ProjectActionTypes.SET_PROJECTS_FEED, { projects })
 
 export const toggleProjectIsLiked = (project: Project) => action(ProjectActionTypes.TOGGLE_PROJECT_IS_LIKED, { project })
 
